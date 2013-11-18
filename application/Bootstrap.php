42f577805f4da9030b0f97e2ba13fdab60fd3e72
ravindraahire <ravi.ahire08@gmail.com>
2013-11-18 11:26:25 +0530

    changes in application.ini and bootstrap file

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,10 +3,6 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-   protected function _initMongo() 
-   {
-      require_once APPLICATION_PATH.'/../library/Bas/Shared/Mongo/Connection.php';
-   }
    
 }
 
